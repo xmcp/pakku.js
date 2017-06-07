bc8807fedb38f27795d44ad58aca12e0e5e21f7c
xmcp <xiaoyuanan2000@sina.com>
2017-06-08 00:02:56 +0800

    add travis-ci test
    will it work this time?

--- test/testrunner.py
+++ test/testrunner.py
@@ -7,7 +7,7 @@ from xml.dom.minidom import parseString
 print('== initializing selenium')
 
 opt=Options()
-opt.add_argument('load-extension=%s'%os.path.abspath('pakkujs'))
+opt.add_argument('load-extension=%s'%os.path.abspath('../pakkujs'))
 opt.add_argument('--no-sandbox')
 opt.add_argument('--no-default-browser-check')
 opt.add_argument('--user-agent=xmcp_pakku_test_runner, like Gecko and Mozilla/5.0')
