010bfeb1ca33852be49150404b4a643cdd20ac59
xmcp <xiaoyuanan2000@sina.com>
2017-06-20 19:57:58 +0800

    v6.1
    improve options page
    close #19

--- pakkujs/options/options.js
+++ pakkujs/options/options.js
@@ -2,6 +2,15 @@ function id(x) {
     return document.getElementById(x);
 }
 
+function try_regexp(x) {
+    try {
+        return new RegExp(x);
+    } catch(e) {
+        alert('正则表达式语法有误：\n\n'+e.message)
+        throw e;
+    }
+}
+
 id('version').textContent='v'+chrome.runtime.getManifest().version;
 var img_btns=document.querySelectorAll('[data-name]');
 
@@ -16,9 +25,9 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
     
     function reload() {
         bgpage.loadconfig();
-        id('saved-alert').classList.remove('hidden');
+        id('saved-alert').classList.remove('saved-hidden');
         setTimeout(function() {
-            id('saved-alert').classList.add('hidden');
+            id('saved-alert').classList.add('saved-hidden');
         },100);
         loadconfig();
     }
@@ -47,26 +56,50 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
                 code1=document.createElement('code'),
                 spliter=document.createElement('span'),
                 code2=document.createElement('code'),
-                deletebtn=document.createElement('button');
+                deletebtn=document.createElement('button'),
+                savebtn=document.createElement('button'),
+                cancelbtn=document.createElement('button');
                 
             code1.textContent=cfg_taolus[i][0].source;
+            code1.contentEditable='plaintext-only';
             spliter.textContent=' → ';
             code2.textContent=cfg_taolus[i][1];
-            deletebtn.textContent='删除';
-            (function(expr) {deletebtn.addEventListener('click',function() {
-                for(var i in cfg_taolus)
-                    if(cfg_taolus[i][0].source==expr) {
-                        delete cfg_taolus[i];
-                        localStorage['TAOLUS']=bgpage.toholyjson(cfg_taolus);
-                        reload();
-                        break;
-                    }
-            })})(cfg_taolus[i][0].source);
+            code2.contentEditable='plaintext-only';
             
-            container.appendChild(deletebtn);
+            deletebtn.textContent='删除';
+            deletebtn.className='btn';
+            savebtn.textContent='保存';
+            savebtn.className='btn hidden';
+            cancelbtn.textContent='取消';
+            cancelbtn.className='btn hidden';
+            
+            (function(index,savebtn,cancelbtn,code1,code2) {
+                deletebtn.addEventListener('click',function() {
+                    delete cfg_taolus[index];
+                    localStorage['TAOLUS']=bgpage.toholyjson(cfg_taolus);
+                    reload();
+                });
+                savebtn.addEventListener('click',function() {
+                    cfg_taolus[index][0]=try_regexp(code1.textContent);
+                    cfg_taolus[index][1]=code2.textContent;
+                    localStorage['TAOLUS']=bgpage.toholyjson(cfg_taolus);
+                    reload();
+                });
+                cancelbtn.addEventListener('click',loadconfig);
+                function show_btn() {
+                    savebtn.classList.remove('hidden');
+                    cancelbtn.classList.remove('hidden');
+                }
+                code1.addEventListener('input',show_btn);
+                code2.addEventListener('input',show_btn);
+            })(i,savebtn,cancelbtn,code1,code2);
+            
             container.appendChild(code1);
             container.appendChild(spliter);
             container.appendChild(code2);
+            container.appendChild(deletebtn);
+            container.appendChild(savebtn);
+            container.appendChild(cancelbtn);
             taolus.appendChild(container);
         }
         
@@ -76,22 +109,43 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
         for(var i in cfg_whitelist) {
             var container=document.createElement('li'),
                 code1=document.createElement('code'),
-                deletebtn=document.createElement('button');
+                deletebtn=document.createElement('button'),
+                savebtn=document.createElement('button'),
+                cancelbtn=document.createElement('button');
                 
             code1.textContent=cfg_whitelist[i][0].source;
-            deletebtn.textContent='删除';
-            (function(expr) {deletebtn.addEventListener('click',function() {
-                for(var i in cfg_whitelist)
-                    if(cfg_whitelist[i][0].source==expr) {
-                        delete cfg_whitelist[i];
-                        localStorage['WHITELIST']=bgpage.toholyjson(cfg_whitelist);
-                        reload();
-                        break;
-                    }
-            })})(cfg_whitelist[i][0].source);
+            code1.contentEditable='plaintext-only';
             
-            container.appendChild(deletebtn);
+            deletebtn.textContent='删除';
+            deletebtn.className='btn';
+            savebtn.textContent='保存';
+            savebtn.className='btn hidden';
+            cancelbtn.textContent='取消';
+            cancelbtn.className='btn hidden';
+            
+            (function(index,savebtn,cancelbtn,code1) {
+                deletebtn.addEventListener('click',function() {
+                    delete cfg_whitelist[index];
+                    localStorage['WHITELIST']=bgpage.toholyjson(cfg_whitelist);
+                    reload();
+                });
+                savebtn.addEventListener('click',function() {
+                    cfg_whitelist[index][0]=try_regexp(code1.textContent);
+                    localStorage['WHITELIST']=bgpage.toholyjson(cfg_whitelist);
+                    reload();
+                });
+                cancelbtn.addEventListener('click',loadconfig);
+                function show_btn() {
+                    savebtn.classList.remove('hidden');
+                    cancelbtn.classList.remove('hidden');
+                }
+                code1.addEventListener('input',show_btn);
+            })(i,savebtn,cancelbtn,code1);
+            
             container.appendChild(code1);
+            container.appendChild(deletebtn);
+            container.appendChild(savebtn);
+            container.appendChild(cancelbtn);
             whitelist.appendChild(container);
         }
         
@@ -106,7 +160,7 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
     id('newtaolu-form').addEventListener('submit',function(e) {
         e.preventDefault();
         cfg_taolus.push([
-            new RegExp(id('newtaolu-pattern').value),
+            try_regexp(id('newtaolu-pattern').value),
             id('newtaolu-name').value
         ]);
         localStorage['TAOLUS']=bgpage.toholyjson(cfg_taolus);
@@ -117,7 +171,7 @@ chrome.runtime.getBackgroundPage(function(bgpage) {
     id('newwhitelist-form').addEventListener('submit',function(e) {
         e.preventDefault();
         cfg_whitelist.push([
-            new RegExp(id('newwhitelist-pattern').value),
+            try_regexp(id('newwhitelist-pattern').value),
             "" // could be anything
         ]);
         localStorage['WHITELIST']=bgpage.toholyjson(cfg_whitelist);
