cfe32d29b684b08a262b372c7b885a0c562bd7e8
xmcp <xiaoyuanan2000@sina.com>
2017-12-02 17:46:30 +0800

    update bas-enabler

--- bas-enabler/bg.js
+++ bas-enabler/bg.js
@@ -1,29 +1,54 @@
-var replaced=`
-<i>
-<chatserver>chat.bilibili.com</chatserver>
-<chatid>12664443</chatid>
-<mission>0</mission>
-<maxlimit>1000</maxlimit>
-<source>k-v</source>
-<d p="1,9,25,16777215,1482851464,0,f571e8a8,2788062125">
-def text foo {
-    content = "foobar"
-    x=10%
-    y=10%
-    color = 0xffffff
+var danmaku=`
+def button foo1 {
+    text="<center><span style='display: inline-block; margin: 0 -20px; font-size: 18px; width: 70px;'>+0.1s</span><br>x123</center>"
+    textColor=0xffffff
+    fillColor=0x0000aa
+    fontSize=10
+    target=seek{time=0s}
+    fillAlpha=.5
+    duration=3s
+    x=0
+    y=30%
 }
-
-set foo {
-    x=50%
-    y=50%
-} 1s
-</d></i>
+def button foo2 {
+    text="<center><span style='display: inline-block; margin: 0 -20px; font-size: 18px; width: 70px;'>+0.1s</span><br>x123</center>"
+    textColor=0xffffff
+    fillColor=0x0000aa
+    fontSize=10
+    target=seek{time=0s}
+    fillAlpha=.5
+    duration=3s
+    x=70
+    y=30%
+}
+def button foo3 {
+    text="<center><span style='display: inline-block; margin: 0 -20px; font-size: 18px; width: 70px;'>+0.1s</span><br>x123</center>"
+    textColor=0xffffff
+    fillColor=0x0000aa
+    fontSize=10
+    target=seek{time=0s}
+    fillAlpha=.5
+    duration=3s
+    x=140
+    y=30%
+}
 `;
 
-/**
+/**/
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-    return {redirectUrl: 'data:text/xml;charset=utf-8,'+encodeURIComponent(replaced)};
+    console.log('replaced');
+    return {redirectUrl: 'data:text/xml;charset=utf-8,'+encodeURIComponent(`
+        <i>
+        <chatserver>chat.bilibili.com</chatserver>
+        <chatid>12664443</chatid>
+        <mission>0</mission>
+        <maxlimit>1000</maxlimit>
+        <source>k-v</source>
+        <d p="1,9,25,16777215,1482851464,0,f571e8a8,2788062125">
+        ${danmaku.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;')}
+        </d>
+    `)};
 }, {urls: ['*://comment.bilibili.com/12664443.xml','*://comment.bilibili.com/rc/12664443.xml']}, ['blocking']);
 
 //*/
