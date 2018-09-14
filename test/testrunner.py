c500059d0c3916784118ba0e8afb031b191d57a0
xmcp <xiaoyuanan2000@sina.com>
2018-09-14 16:15:08 +0800

    update test

--- test/testrunner.py
+++ test/testrunner.py
@@ -9,7 +9,7 @@ print('== initializing selenium')
 opt=Options()
 opt.add_argument('load-extension=%s'%os.path.abspath('../dist/C'))
 opt.add_argument('--no-default-browser-check')
-opt.add_argument('--user-agent=xmcp_pakku_test_runner, like Gecko and Mozilla/5.0, actually something like Chrome/64.0.3282.186 Safari/537.36')
+opt.add_argument('--user-agent=xmcp_pakku_test_runner Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36')
 opt.add_argument('--no-first-run')
 b=webdriver.Chrome('driver/chromedriver',chrome_options=opt)
 
