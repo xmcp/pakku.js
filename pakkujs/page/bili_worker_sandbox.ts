c3f6ac4c569649dd503ffe703be91ffb7467401a
xmcp <xmcp@pku.edu.cn>
2024-04-11 02:42:19 +0800

    fix content script naming conflict

--- pakkujs/page/bili_worker_sandbox.ts
+++ pakkujs/page/bili_worker_sandbox.ts
@@ -1,34 +1,35 @@
 import {UserscriptWorker} from "../core/userscript";
 
-document.close();
+(()=>{ // wrap in iife to avoid name conflict with content_script.js
 
-if(window !== window.parent) {
-    console.log('pakku sandbox: injected');
-    let ext_domain = chrome.runtime.getURL('');
-    if(ext_domain.endsWith('/'))
-        ext_domain = ext_domain.slice(0, -1);
+    if(window !== window.parent) {
+        console.log('pakku sandbox: injected');
+        let ext_domain = chrome.runtime.getURL('');
+        if(ext_domain.endsWith('/'))
+            ext_domain = ext_domain.slice(0, -1);
 
-    window.onmessage = (e) => {
-        if(e.origin !== ext_domain) {
-            console.error('pakku sandbox: origin', e.origin, 'does not match', ext_domain);
-            return;
-        }
+        window.onmessage = (e) => {
+            if(e.origin !== ext_domain) {
+                console.error('pakku sandbox: origin', e.origin, 'does not match', ext_domain);
+                return;
+            }
 
-        console.log('pakku sandbox: received request', e.data);
+            console.log('pakku sandbox: received request', e.data);
 
-        if(e.data.type==='pakku_sandbox_request') {
-            (async ()=>{
-                let w = null;
-                try {
-                    w = new UserscriptWorker(e.data.script);
-                    let res = await w.init();
-                    window.parent.postMessage({type: 'pakku_sandbox_result', error: null, total: res[0]+res[1]}, ext_domain);
-                } catch(e) {
-                    window.parent.postMessage({type: 'pakku_sandbox_result', error: `${(e as Error).message || '未知错误'}\n\n${(e as Error).stack}`}, ext_domain);
-                }
-                if(w)
-                    w.worker.terminate();
-            })();
-        }
-    };
-}
+            if(e.data.type==='pakku_sandbox_request') {
+                (async ()=>{
+                    let w = null;
+                    try {
+                        w = new UserscriptWorker(e.data.script);
+                        let res = await w.init();
+                        window.parent.postMessage({type: 'pakku_sandbox_result', error: null, total: res[0]+res[1]}, ext_domain);
+                    } catch(e) {
+                        window.parent.postMessage({type: 'pakku_sandbox_result', error: `${(e as Error).message || '未知错误'}\n\n${(e as Error).stack}`}, ext_domain);
+                    }
+                    if(w)
+                        w.worker.terminate();
+                })();
+            }
+        };
+    }
+})();
--- pakkujs/page/parse_local.js
+++ pakkujs/page/parse_local.js
@@ -1,83 +1,86 @@
-let $container = document.querySelector('#result-container');
-let $download_btn = document.querySelector('#download-btn');
-let $clear_btn = document.querySelector('#clear-btn');
+(()=>{ // wrap in iife to avoid name conflict with content_script.js
 
-function download(filename, text) {
-    let a = document.createElement('a');
-    a.href = URL.createObjectURL(new Blob([text], {type: 'application/octet-stream; charset=utf-8'}));
-    a.download = filename;
-    a.click();
-}
+    let $container = document.querySelector('#result-container');
+    let $download_btn = document.querySelector('#download-btn');
+    let $clear_btn = document.querySelector('#clear-btn');
 
-let all_res = [];
-
-$download_btn.onclick = function() {
-    for(let [filename, res] of all_res) {
-        download(filename, res);
-    }
-};
-$clear_btn.onclick = function() {
-    for(let fin of $container.querySelectorAll('.finished')) {
-        fin.parentElement.removeChild(fin);
-    }
-    all_res = [];
-};
-
-async function do_process(filename, ext, ingress, egress) {
-    let row = document.createElement('p');
-    row.textContent = `${filename}：处理中…`;
-    $container.appendChild(row);
-
-    let res = await window.pakku_process_local(ingress, egress);
-
-    all_res.push([`${filename}_pakku.${ext}`, res.data]);
-    row.classList.add('finished');
-    row.textContent = `${filename}：处理完成（${ingress.type} → ${egress.type} ${(res.data.length/1024).toFixed(0)} KB）`;
-}
-
-function read_file(file) {
-    return new Promise((resolve, reject) => {
-        let fr = new FileReader();
-        fr.onload = function() {
-            resolve(fr.result);
-        };
-        fr.readAsText(file, 'utf-8');
-    });
-}
-
-document.body.addEventListener('dragenter', function() {
-    window.drag_count++;
-    document.body.classList.add('dragged');
-});
-document.body.addEventListener('dragleave', function() {
-    if(--window.drag_count === 0)
-        document.body.classList.remove('dragged');
-});
-document.body.addEventListener('dragover', function(e) {
-    e.preventDefault();
-});
-document.body.addEventListener('drop', function(e) {
-    e.preventDefault();
-    window.drag_count = 0;
-    document.body.classList.remove('dragged');
-
-    let options = {};
-    for(let input of document.querySelectorAll('input[type=radio]:checked')) {
-        options[input.name] = input.value;
+    function download(filename, text) {
+        let a = document.createElement('a');
+        a.href = URL.createObjectURL(new Blob([text], {type: 'application/octet-stream; charset=utf-8'}));
+        a.download = filename;
+        a.click();
     }
 
-    let ingress_type = options.ingress;
-    let egress = options.egress==='xml' ? {type: 'xml'} : options.egress==='debug' ? {type: 'debug', show_peers: false} : {type: 'debug', show_peers: true};
-    let ext = options.egress==='xml' ? 'xml' : 'js';
+    let all_res = [];
 
-    let files = e.dataTransfer.files;
-
-    let perform = async function() {
-        for(let file of files) {
-            let content = await read_file(file);
-            await do_process(file.name, ext, {type: ingress_type, content: content}, egress);
+    $download_btn.onclick = function() {
+        for(let [filename, res] of all_res) {
+            download(filename, res);
         }
     };
+    $clear_btn.onclick = function() {
+        for(let fin of $container.querySelectorAll('.finished')) {
+            fin.parentElement.removeChild(fin);
+        }
+        all_res = [];
+    };
 
-    void perform();
-});
+    async function do_process(filename, ext, ingress, egress) {
+        let row = document.createElement('p');
+        row.textContent = `${filename}：处理中…`;
+        $container.appendChild(row);
+
+        let res = await window.pakku_process_local(ingress, egress);
+
+        all_res.push([`${filename}_pakku.${ext}`, res.data]);
+        row.classList.add('finished');
+        row.textContent = `${filename}：处理完成（${ingress.type} → ${egress.type} ${(res.data.length/1024).toFixed(0)} KB）`;
+    }
+
+    function read_file(file) {
+        return new Promise((resolve, reject) => {
+            let fr = new FileReader();
+            fr.onload = function() {
+                resolve(fr.result);
+            };
+            fr.readAsText(file, 'utf-8');
+        });
+    }
+
+    document.body.addEventListener('dragenter', function() {
+        window.drag_count++;
+        document.body.classList.add('dragged');
+    });
+    document.body.addEventListener('dragleave', function() {
+        if(--window.drag_count === 0)
+            document.body.classList.remove('dragged');
+    });
+    document.body.addEventListener('dragover', function(e) {
+        e.preventDefault();
+    });
+    document.body.addEventListener('drop', function(e) {
+        e.preventDefault();
+        window.drag_count = 0;
+        document.body.classList.remove('dragged');
+
+        let options = {};
+        for(let input of document.querySelectorAll('input[type=radio]:checked')) {
+            options[input.name] = input.value;
+        }
+
+        let ingress_type = options.ingress;
+        let egress = options.egress==='xml' ? {type: 'xml'} : options.egress==='debug' ? {type: 'debug', show_peers: false} : {type: 'debug', show_peers: true};
+        let ext = options.egress==='xml' ? 'xml' : 'js';
+
+        let files = e.dataTransfer.files;
+
+        let perform = async function() {
+            for(let file of files) {
+                let content = await read_file(file);
+                await do_process(file.name, ext, {type: ingress_type, content: content}, egress);
+            }
+        };
+
+        void perform();
+    });
+})();
