96ba5256924cd018f2da5958337f6f4080835c6f
xmcp <xmcp@pku.edu.cn>
2024-04-11 18:13:55 +0800

    fix content script naming conflict in xhr_hook_injector

--- pakkujs/assets/xhr_hook_injector.js
+++ pakkujs/assets/xhr_hook_injector.js
@@ -1,6 +1,8 @@
 // used for firefox
 
-console.log('pakku: inject xhr hook');
-let sc = document.createElement('script');
-sc.src = chrome.runtime.getURL('/assets/xhr_hook.js');
-document.documentElement.appendChild(sc);
+(()=>{ // wrap in iife to avoid name conflict with content_script.js
+    console.log('pakku: inject xhr hook');
+    let sc = document.createElement('script');
+    sc.src = chrome.runtime.getURL('/assets/xhr_hook.js');
+    document.documentElement.appendChild(sc);
+})();
