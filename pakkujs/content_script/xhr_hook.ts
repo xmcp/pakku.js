8f125cf9b5eb2e6c4a1c42cea3de8429bd137326
xmcp <xiaoyuanan2000@sina.com>
2025-11-30 00:22:39 +0800

    bump version to 2025.12.1

--- pakkujs/content_script/xhr_hook.ts
+++ pakkujs/content_script/xhr_hook.ts
@@ -1,3 +1,6 @@
+// noinspection JSConstantReassignment
+// ↑ for rewriting things like xhr.response
+
 import {AjaxResponse} from "../core/types";
 
 declare global {
@@ -315,7 +318,10 @@ type MutableXMLHttpRequest = Mutable<XMLHttpRequest>;
             if(event.data.type === 'pakku_video_jump') {
                 window.player?.seek(event.data.time);
             } else if(event.data.type === 'pakku_trigger_reload') {
-                // xxx: not working in the latest player (#322)
+                // xxx: causing problems in the latest player (#322 #323)
+                // will probably add this feature back when bilibili has fixed its player
+                // also we might want to restrict to only reload the active tabid
+
                 //window.player?.reload(null);
             }
         }
